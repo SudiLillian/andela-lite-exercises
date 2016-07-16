07b78dd85ed2da6e263b96d98c05d37c3064f746
Lilian Sudi <liliandebysudi@gmail.com>
2016-07-16 14:32:22 +0300

    updated the student dictionary

--- students.py
+++ students.py
@@ -1,15 +1,14 @@
+from datetime import date
+
 student = {'name':["Lillian", "Sudi"], 'DOB': 1997,'phone_number': '0713100894' }
 
 a=student.get('name')
-
-print a
-
 b=student.get('DOB')
-
-print b
-
 c=student.get('phone_number')
+age= date.today().year-b
 
-print c
 
-age
+print 'Name; {0}'.format(a)
+print 'Year of birth; {0}'.format(b)
+print 'Phone number; {0}'.format(c)
+print 'Age; {0}'.format(age)
