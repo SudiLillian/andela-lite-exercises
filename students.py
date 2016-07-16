c9605731d7c81562ad2adcb120527a2bfec44b28
Lilian Sudi <liliandebysudi@gmail.com>
2016-07-16 14:12:30 +0300

    updated student details

--- students.py
+++ students.py
@@ -1,1 +1,15 @@
-student = {}
+student = {'name':["Lillian", "Sudi"], 'DOB': 1997,'phone_number': '0713100894' }
+
+a=student.get('name')
+
+print a
+
+b=student.get('DOB')
+
+print b
+
+c=student.get('phone_number')
+
+print c
+
+age
